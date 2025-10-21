78647784aed57ecd71deb7530174bffc5d26d840
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-20 20:28:30 -0700

    draging is hard

--- Scenes/pet.gd
+++ Scenes/pet.gd
@@ -1,16 +1,29 @@
 extends AnimatedSprite2D
 
+# 🐾 Variables for dragging
+var able_drag: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Make window clickable + visible
+	get_window().transparent = true
 	get_window().mouse_passthrough = false
-	#get_window().mouse_passthrough_polygon = $Polygon2D.polygon
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	get_window().always_on_top = true
+
 func _process(delta: float) -> void:
-	pass
+	# When dragging, move window to follow the cursor
+	if able_drag:
+		get_window().position = Vector2i(get_global_mouse_position() - drag_offset)
 
-
+# 🖱️ Called when you interact with the Area2D around the sprite
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		print("hello")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			# Start dragging when mouse is pressed
+			able_drag = true
+			drag_offset = (get_global_mouse_position() - Vector2(get_window().position)) * 0.5
+
+			print("hello")  # Optional feedback when clicked
+		else:
+			# Stop dragging when mouse is released
+			able_drag = false
