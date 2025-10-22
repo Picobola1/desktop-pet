2f20c7a97d99f993ab0bf6d5406a9cdf9f298678
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-21 19:32:27 -0700

    cleaner version

--- Scenes/pet.gd
+++ Scenes/pet.gd
@@ -1,32 +1,15 @@
 extends Node2D
 
 
-@onready var MyPolygon: Polygon2D = $Polygon2D
 
 @onready var pet: AnimatedSprite2D = $Pet
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().process_frame
-	get_window().transparent = true
-	get_window().always_on_top = true
-	#get_window().mouse_passthrough = false
-	#get_window().mouse_passthrough_polygon = MyPolygon.polygon
-	get_window().size = pet.sprite_frames.get_frame_texture(pet.animation, 0).get_size()
-
 	
+	get_window().size = pet.sprite_frames.get_frame_texture(pet.animation, 0).get_size()
+	get_window().position = Vector2i(0,0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	#get_window().mouse_passthrough_polygon = MyPolygon.polygon
+func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
-			print("hello")
-func _input(event: InputEvent) -> void:
-	pass
-	#if event is InputEventMouseButton:
-		#print("hello")
+			print("IT CLICKS")
