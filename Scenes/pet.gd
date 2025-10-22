0f7df40ff08f03a1015c3aa6770fb9184d250220
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-21 20:06:51 -0700

    hehe he is a big boy now and is on the taskbar

--- Scenes/pet.gd
+++ Scenes/pet.gd
@@ -4,10 +4,16 @@ extends Node2D
 
 @onready var pet: AnimatedSprite2D = $Pet
 
+
+
 func _ready() -> void:
-	
-	get_window().size = pet.sprite_frames.get_frame_texture(pet.animation, 0).get_size()
-	get_window().position = Vector2i(0,0)
+	var petScale = pet.scale
+	petScale = Vector2(3,3)
+	pet.scale = petScale
+	get_window().size = Vector2i(pet.sprite_frames.get_frame_texture(pet.animation, 0).get_size()* petScale)
+	var ScreenSize = DisplayServer.screen_get_size()
+	var window_size = get_window().size
+	get_window().position = ScreenSize - window_size - Vector2i(0,70) 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
