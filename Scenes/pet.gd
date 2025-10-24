be0fdfa66f30932fefa883998367d4be383c65bd
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-23 18:36:03 -0700

    hehe he go zoom

--- Scenes/pet.gd
+++ Scenes/pet.gd
@@ -4,10 +4,11 @@ extends Node2D
 
 @onready var pet: AnimatedSprite2D = $Pet
 
-var clicks = 0
+
 
 func _ready() -> void:
 	var petScale = pet.scale
+
 	petScale = Vector2(2,2)
 	pet.scale = petScale
 	get_window().size = Vector2i(pet.sprite_frames.get_frame_texture(pet.animation, 0).get_size()* petScale)
@@ -17,21 +18,17 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if clicks == 0:
-			if event.pressed:
-				clicks += 1
-				for i in range(355):
-					get_window().position += Vector2i(-5,0)
-					pet.play("walk")
-					await get_tree().process_frame
-				pet.flip_h = false
-				await get_tree().create_timer(1.0).timeout
-				pet.play("idle")
-		elif clicks == 1:
-			clicks = 0
-			for i in range(355):
-				get_window().position += Vector2i(5,0)
-				pet.play("walk")
-				await get_tree().process_frame
-			pet.flip_h = true
-			pet.play("idle")
+		if event.pressed:
+			ToEdge(360,true)
+			
+				
+			
+func ToEdge(range: int,direction: bool):
+	for i in range(range):
+		if direction == false:
+			get_window().position += Vector2i(5,0)
+		else:
+			get_window().position += Vector2i(-5,0)
+	pet.play("walk")
+
+	await get_tree().process_frame
